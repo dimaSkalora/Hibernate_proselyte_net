c37733856f24cb9eb73301d2121bfdf9ac304de3
dimaSkalora <timon2@ukr.net>
2017-09-26 17:13:38 +0300

    11. Запросы с использованием Criteria.

--- src/main/java/lesson_11/Developer.java
+++ src/main/java/lesson_11/Developer.java
@@ -1,22 +1,13 @@
-package lesson_09;
+package lesson_11;
 
-import javax.persistence.*;
 
-@Entity
-@Table(name = "HIBERNATE_DEVELOPERS")
 public class Developer {
-    @Id
-    @GeneratedValue (strategy = GenerationType.AUTO)
-    @Column (name = "id")
     private int id;
-    @Column (name = "FIRST_NAME")
     private String firstName;
-    @Column (name = "LAST_NAME")
     private String lastName;
-    @Column (name = "SPECIALTY")
     private String specialty;
-    @Column (name = "EXPERIENCE")
     private int experience;
+    private int salary;
 
     /**
      * Default Constructor
@@ -27,11 +18,12 @@ public class Developer {
     /**
      * Plain constructor
      */
-    public Developer(String firstName, String lastName, String specialty, int experience) {
+    public Developer(String firstName, String lastName, String specialty, int experience, int salary) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.specialty = specialty;
         this.experience = experience;
+        this.salary = salary;
     }
 
     /**
@@ -77,16 +69,24 @@ public class Developer {
         this.experience = experience;
     }
 
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
     /**
      * toString method (optional)
      */
     @Override
     public String toString() {
-        return "Developer:\n" +
-                "id: " + id +
-                "\nFirst Name: " + firstName + "\n" +
-                "Last Name: " + lastName + "\n" +
-                "Specialty: " + specialty + "\n" +
-                "Experience: " + experience + "\n";
+        return "id: " + id +
+                "\nFirst Name: " + firstName +
+                "\nLast Name: " + lastName +
+                "\nSpecialty: " + specialty +
+                "\nExperience: " + experience +
+                "\nSalary: " + salary + "\n";
     }
 }
